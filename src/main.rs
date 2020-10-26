4d711a8e2c9cd42648324f6b0682241932579849
danitrod <danitrod@ibm.com>
2020-10-26 20:02:54 -0300

    Modularize code, add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("Not enough arguments (minimum 2)");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+        process::exit(1);
     }
 }
