f0795559d13d2db4cc909fc9071d4ac92e198498
danitrod <danitrod@ibm.com>
2020-10-29 22:06:33 -0300

    Add stderr logging

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
